cb55593506230f78e687c98b7e016bc7c4fea260
angel <angel@LAPTOP-TUOF9OKV>
2024-02-13 02:42:47 -0600

    Agregar archivos de proyecto.

---
+++ riojas unidad 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("sumar dos numeros");
+Console.ReadLine();
+Console.WriteLine("dame el primer numero:");
+double numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("dame el segundo numero:");
+double numero2 = int.Parse(Console.ReadLine());
+double resultado = numero1 + numero2;
+Console.WriteLine("el resultado es:" + resultado);
+Console.ReadKey();
+
